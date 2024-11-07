fb11b78a1bd2b9a46655a38fdb8a9c7c7580c5be
Vans <stevanstc2018@gmail.com>
2024-11-07 14:00:38 +0700

    Initial Commit

---
+++ BudSyncRework/BudSyncReworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudSyncReworkApp.swift
+//  BudSyncRework
+//
+//  Created by Stevans Calvin Candra on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BudSyncReworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
